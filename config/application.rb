8b538f3f933c3b58bd4d90c12c3e8530f25c17ae
k_nakatsuka <hakkou218@gmail.com>
2015-10-16 10:05:52 +0000

    add top page

--- config/application.rb
+++ config/application.rb
@@ -14,8 +14,6 @@ module Microposts
       g.helper false
       g.test_framework false
     end
-  end
-end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
